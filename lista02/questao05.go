31d2dece70c0d246ab119f6ab18dc3170c3b06dc
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-03-25 21:46:18 -0300

    questao05

--- lista02/questao05.go
+++ lista02/questao05.go
@@ -1,1 +1,17 @@
+package main
+import f "fmt"
 
+func main() {
+    
+    var numero int
+    
+    f.Println("Digite um número do tipo inteiro: ")
+    f.Scan(&numero)
+    f.Println("\n")
+    
+    if numero % 5 == 0 {
+        f.Println("O número é divisível por 5.")
+    } else {
+        f.Println("O número não é divisível por 5.")
+    }
+}
