1984c59a13a459e71bf6dc0ace7e8888bb7463ad
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-03-25 21:09:58 -0300

    questao01

--- lista02/questao01.go
+++ lista02/questao01.go
@@ -1,1 +1,18 @@
+package main
+import  "fmt"
 
+func main() {
+    
+    var numero int
+    
+    fmt.Println("Digite um número inteiro: ")
+    fmt.Scan(&numero)
+    fmt.Println("\n")
+
+    if numero % 2 == 0 {
+        fmt.Println("O número digitado é par.")
+    } else {
+         fmt.Println("O número digitado é ímpar.")
+    }
+
+}
