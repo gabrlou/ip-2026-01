49064d69f22cd2e5866f10c6674d8b648f11b447
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-03-25 21:20:20 -0300

    questao03

--- lista02/questao03.go
+++ lista02/questao03.go
@@ -1,1 +1,22 @@
+package main
+import  "fmt"
 
+func main() {
+    
+    var n1, n2, soma int
+    
+    fmt.Println("Digite um primeiro número inteiro: ")
+    fmt.Scan(&n1)
+    
+    fmt.Println("Digite um segundo número inteiro: ")
+    fmt.Scan(&n2)
+    fmt.Println("\n")
+    
+    soma = n1 + n2
+    if soma > 20 {
+        fmt.Println("O resultado é: ", soma + 8)
+    } else {
+         fmt.Println("O resultado é: ", soma - 5)
+    }
+
+}
