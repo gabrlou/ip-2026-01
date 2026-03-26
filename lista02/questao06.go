3a44d34acc353d4f5fe6d5cdb3964eee03204189
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-03-25 21:51:20 -0300

    questao06

--- lista02/questao06.go
+++ lista02/questao06.go
@@ -1,1 +1,20 @@
+package main
+import f "fmt"
 
+func main() {
+    
+    var a, b int
+    
+    f.Println("Digite um número inteiro \"A\": ")
+    f.Scan(&a)
+    
+    f.Println("Digite um número inteiro \"B\": ")
+    f.Scan(&b)
+    f.Println("\n")
+    
+    if a % b == 0 {
+        f.Println("O número A = ", a, " é divisível pelo número B = ", b )
+    } else {
+        f.Println("Os números não são divisíveis um pelo outro.")
+    }
+}
