99b84c6e481d39bcba99e9ece6ab76e54a857b9d
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-03-25 21:13:58 -0300

    questao02

--- lista02/questao02.go
+++ lista02/questao02.go
@@ -1,1 +1,20 @@
+package main
+import  "fmt"
 
+func main() {
+    
+    var numero int
+    
+    fmt.Println("Digite um número inteiro: ")
+    fmt.Scan(&numero)
+    fmt.Println("\n")
+
+    if numero < 0 {
+        fmt.Println("O número digitado é negativo.")
+    } else if numero > 0 {
+         fmt.Println("O número digitado é positivo.")
+    } else {
+        fmt.Println("O número digitado é nulo.")
+    }
+
+}
