e6f05dbb3baf7a940696c19a187802b41977d973
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-03-25 21:42:15 -0300

    questao04

--- lista02/questao04.go
+++ lista02/questao04.go
@@ -1,1 +1,24 @@
+package main
+import (
+    "fmt"
+    "math"
+)
 
+func main() {
+    
+    var numero, raiz, quadrado float64
+    
+    fmt.Println("Digite um número do tipo float: ")
+    fmt.Scan(&numero)
+    fmt.Println("\n")
+    
+    if numero > 0 || numero == 0 {
+        raiz = math.Sqrt(numero)
+        fmt.Printf("A raiz quadrada do número é: %.2f\n", raiz)
+        
+    } else {
+        quadrado = numero * numero
+        fmt.Println("O quadrado do número é: ", quadrado)
+    }
+
+}
