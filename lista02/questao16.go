a0ad4b7510bab4d907842f921fd35f13a46ea6ee
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-03-31 03:07:04 -0300

    Resolução questões 16 a 18

--- lista02/questao16.go
+++ lista02/questao16.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	f "fmt"
+	m "math"
+)
 
+func main() {
+	var A, B, C, delta, raiz_1, raiz_2, parte_a, parte_b float64
+
+	f.Print("Digite o valor de A:")
+	f.Scanln(&A)
+
+	f.Print("Digite o valor de B:")
+	f.Scanln(&B)
+
+	f.Print("Digite o valor de C:")
+	f.Scanln(&C)
+
+	delta = (B * B) - (4 * A * C)
+	if A == 0 { //Verificar se é uma equação do 2º Grau
+		f.Println("\nValores inválidos!\nNão é uma equação do 2º Grau.")
+		return
+	} else if delta < 0 { //RAIZES IMAGINARIAS
+		parte_a = (-B) / (2 * A)
+		parte_b = (m.Sqrt(-delta)) / (2 * A)
+
+		if parte_a == 0 {
+			f.Printf("\nValor de x1: + %.2fi\nValor de x2: - %.2fi", parte_b, parte_b)
+			f.Println("\nClassificação: RAÍZES IMAGINÁRIAS")
+		} else {
+			f.Printf("\nValor de x1: %.2f + %.2fi\nValor de x2: %.2f - %.2fi", parte_a, parte_b, parte_a, parte_b)
+			f.Println("\nClassificação: RAÍZES IMAGINÁRIAS")
+		}
+	} else if delta == 0 { //RAIZ UNICA
+		raiz_1 = (-B) / (2 * A)
+
+		f.Printf("\nValor de x1 e x2: %.2f", raiz_1)
+		f.Println("\nClassificação: RAÍZ ÚNICA")
+	} else { //RAIZES DISTINTAS
+		raiz_1 = (-B + m.Sqrt(delta)) / (2 * A)
+		raiz_2 = (-B - m.Sqrt(delta)) / (2 * A)
+
+		f.Printf("\nValor de x1: %.2f\nValor de x2: %.2f", raiz_1, raiz_2)
+		f.Println("\nClassificação: RAÍZES DISTINTAS")
+	}
 }
--- lista02/questao18.go
+++ lista02/questao18.go
@@ -1,4 +1,45 @@
 package main
+
 import f "fmt"
 
 func main() {
+	var (
+		preco_normal, preco_final float64
+		dia_semana, categoria     int
+	)
+
+	f.Print("Digite o preço normal de locação do DVD: ")
+	f.Scanln(&preco_normal)
+
+	f.Print("Digite o dia da semana (\"1\" para domingo,\"2\" para segunda, \"3\" para terça, \n\"4\" para quarta, \"5\" para quinta, \"6\"para sexta ou \"7\" para sábado): ")
+	f.Scanln(&dia_semana)
+	if dia_semana < 1 || dia_semana > 7 {
+		f.Print("\nValores inválidos!\nDigite \"1\" para domingo,\"2\" para segunda, \"3\" para terça, \n\"4\" para quarta, \"5\" para quinta, \"6\"para sexta ou \"7\" para sábado.")
+		return
+	}
+
+	f.Print("Digite qual a categoria do DVD (\"1\" para comum ou \"2\" para lançamento): ")
+	f.Scanln(&categoria)
+	if categoria != 1 && categoria != 2 {
+		f.Print("\nValores inválidos!\nDigite \"1\" para comum ou \"2\" para lançamento.")
+		return
+	}
+
+	if categoria == 1 { //Categoria comum
+		if dia_semana == 4 || dia_semana == 6 || dia_semana == 7 || dia_semana == 1 { //Dia normal
+			preco_final = preco_normal
+			f.Printf("\nO preço de locação é de: R$ %.2f", preco_final)
+		} else { //Dia com desconto
+			preco_final = preco_normal - (preco_normal * 0.4)
+			f.Printf("\nO preço de locação é de: R$ %.2f", preco_final)
+		}
+	} else { //Categoria de lançamento
+		preco_final = preco_normal + (preco_normal * 0.15)
+		if dia_semana == 4 || dia_semana == 6 || dia_semana == 7 || dia_semana == 1 { //Dia normal
+			f.Printf("\nO preço de locação é de: R$ %.2f", preco_final)
+		} else { //Dia com desconto
+			preco_final -= (preco_final * 0.4)
+			f.Printf("\nO preço de locação é de: R$ %.2f", preco_final)
+		}
+	}
+}
