b8a04753842588dccc861a0594c7879ebaf864fe
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-01 19:44:12 -0300

    Resolução exercício 21

--- lista02/questao21.go
+++ lista02/questao21.go
@@ -25,14 +25,14 @@ func main() {
 	media_final = (n1 + (n2 * 2) + (n3 * 3) + media_ex) / 7
 
 	if media_final < 4 {
-		f.Printf("\nIdentificação do aluno: %d\nMédia de aproveitamento: %.2f\nConceito: E\n", id, media_final)
+		f.Printf("\nIdentificação do aluno: %d\nNota 1: %.2f\nNota 2: %.2f\nNota 3: %.2f\nMedia obtida nos exercícios: %.2f\nMédia de aproveitamento: %.2f\nConceito: E\nREPROVADO\n", id, n1, n2, n3, media_ex, media_final)
 	} else if media_final >= 4 && media_final < 6 {
-		f.Printf("\nIdentificação do aluno: %d\nMédia de aproveitamento: %.2f\nConceito: D\n", id, media_final)
+		f.Printf("\nIdentificação do aluno: %d\nNota 1: %.2f\nNota 2: %.2f\nNota 3: %.2f\nMedia obtida nos exercícios: %.2f\nMédia de aproveitamento: %.2f\nConceito: D\nREPROVADO\n", id, n1, n2, n3, media_ex, media_final)
 	} else if media_final >= 6 && media_final < 7.5 {
-		f.Printf("\nIdentificação do aluno: %d\nMédia de aproveitamento: %.2f\nConceito: C\n", id, media_final)
+		f.Printf("\nIdentificação do aluno: %d\nNota 1: %.2f\nNota 2: %.2f\nNota 3: %.2f\nMedia obtida nos exercícios: %.2f\nMédia de aproveitamento: %.2f\nConceito: C\nAPROVADO\n", id, n1, n2, n3, media_ex, media_final)
 	} else if media_final >= 7.5 && media_final < 9 {
-		f.Printf("\nIdentificação do aluno: %d\nMédia de aproveitamento: %.2f\nConceito: B\n", id, media_final)
+		f.Printf("\nIdentificação do aluno: %d\nNota 1: %.2f\nNota 2: %.2f\nNota 3: %.2f\nMedia obtida nos exercícios: %.2f\nMédia de aproveitamento: %.2f\nConceito: B\nAPROVADO\n", id, n1, n2, n3, media_ex, media_final)
 	} else {
-		f.Printf("\nIdentificação do aluno: %d\nMédia de aproveitamento: %.2f\nConceito: A\n", id, media_final)
+		f.Printf("\nIdentificação do aluno: %d\nNota 1: %.2f\nNota 2: %.2f\nNota 3: %.2f\nMedia obtida nos exercícios: %.2f\nMédia de aproveitamento: %.2f\nConceito: A\nAPROVADO\n", id, n1, n2, n3, media_ex, media_final)
 	}
 }
