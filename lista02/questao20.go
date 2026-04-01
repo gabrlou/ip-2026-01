77267a22af01f44b0764783d025af50b7b959877
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-01 19:03:35 -0300

    Resolução exercício 20

--- lista02/questao20.go
+++ lista02/questao20.go
@@ -2,3 +2,31 @@ package main
 import f "fmt"
 
 func main() {
+	var (
+		preco_normal, preco_final float64
+		tipo_pagamento int
+	)
+
+	f.Print("Digite o preço normal de venda: ")
+	f.Scanln(&preco_normal)
+
+	f.Print("Digite a forma de pagamento:\n1 - À vista, dinheiro ou cheque\n2 - À vista ou cartão de crédito\n3 - Em 2 vezes\n4 - Em 3 vezes\n")
+	f.Scanln(&tipo_pagamento)
+
+	if tipo_pagamento < 1 || tipo_pagamento > 4 {
+		f.Print("\nDígito inválido!\n1 - À vista, dinheiro ou cheque\n2 - À vista ou cartão de crédito\n3 - Em 2 vezes\n4 - Em 3 vezes")
+		return
+	} else if tipo_pagamento == 1 { //Tipo 1
+		preco_final = preco_normal - (preco_normal*0.1)
+		f.Printf("\nValor a ser pago: R$ %.2f", preco_final)
+	} else if tipo_pagamento == 2 { //Tipo 2
+		preco_final = preco_normal - (preco_normal*0.05)
+		f.Printf("\nValor a ser pago: R$ %.2f", preco_final)
+	} else if tipo_pagamento == 3 { //Tipo 3
+		preco_final = preco_normal
+		f.Printf("\nValor a ser pago: R$ %.2f", preco_final)
+	} else { //Tipo 4
+		preco_final = preco_normal + (preco_normal*0.1)
+		f.Printf("\nValor a ser pago: R$ %.2f", preco_final)
+	}
+}
