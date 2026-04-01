c605cdc58a6ef8019608cabf9e5ab80eaf93e6e8
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-01 20:48:49 -0300

    Resolução do exercício 03

--- sala-de-aula/aula08/exercicio03.go
+++ sala-de-aula/aula08/exercicio03.go
@@ -1,1 +1,20 @@
+package main
+import f  "fmt"
+func main() {
+   
+   n1, n2, n3 := 0, 0, 0
+   f.Print("Digite o primeiro número: ")
+   f.Scan(&n1)
+   f.Print("Digite o segundo número: ")
+   f.Scan(&n2)
+   f.Print("Digite o terceiro número: ")
+   f.Scan(&n3)
+   
+   result := MEDIA(n1,n2,n3)
+   f.Printf("\nA média entre os números %d, %d e %d é: %.2f\n", n1, n2, n3, result)
+}
 
+func MEDIA(numero1, numero2, numero3 int) float64 {
+	media := float64(numero1 + numero2 + numero3) / 3
+	return media
+}
