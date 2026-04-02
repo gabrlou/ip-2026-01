65f484f3a933b53fd3eff6ff0142580b85a43f3c
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-02 01:46:38 -0300

    Resolução exercício 23

--- lista02/questao23.go
+++ lista02/questao23.go
@@ -1,4 +1,20 @@
 package main
+
 import f "fmt"
 
 func main() {
+	var idade int
+
+	f.Print("Digite sua idade: ")
+	f.Scanln(&idade)
+
+	if idade < 0 {
+		f.Print("\nIdade inválida!\nDigite uma idade maior que zero.")
+	} else if idade < 16 {
+		f.Print("\nClasse eleitoral: Não-eleitor")
+	} else if (idade >= 16 && idade < 18) || idade > 65 {
+		f.Print("\nClasse eleitoral: Eleitor facultativo")
+	} else {
+		f.Print("\nClasse eleitoral: Eleitor obrigatório")
+	}
+}
