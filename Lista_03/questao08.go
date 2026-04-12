1b91e8b34b3df19cac60469b41d8e55ae1a6ea63
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-12 02:00:46 -0300

    Resolução Lista 03

--- Lista_03/questao08.go
+++ Lista_03/questao08.go
@@ -1,6 +1,14 @@
 package main
+
 import f "fmt"
 
 func main() {
-	var teste
+	var soma = 0
+
+	f.Print("\n")
+	for i := 1; i <= 20; i++ {
+		f.Printf("%d ", i)
+		soma += i
+	}
+	f.Printf("\nSoma: %d\n\n", soma)
 }
