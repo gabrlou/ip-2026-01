9c7744e802939d344e25c0ba423f1cc9493ec313
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-14 01:57:57 -0300

    Resolução Lista 03

--- Lista_03/questao21.go
+++ Lista_03/questao21.go
@@ -1,6 +1,39 @@
 package main
+
 import f "fmt"
 
 func main() {
-	var teste
+	var b, n int
+
+	for {
+		f.Print("\nDigite o valor da base b: ")
+		f.Scan(&b)
+		if b < 2 {
+			f.Print("\nNúmero inválido. Digite um número maior ou igual a 2 para a base.\n")
+			continue
+		} else {
+			break
+		}
+	}
+
+	for {
+		f.Print("Digite o valor do expoente n: ")
+		f.Scan(&n)
+		if n <= 1 {
+			f.Print("\nNúmero inválido. Digite um número maior que 1 para o expoente.\n")
+			continue
+		} else {
+			break
+		}
+	}
+
+	resultado := potencia(b, n)
+	f.Printf("\n%d ^ %d = %d\n\n", b, n, resultado)
+}
+
+func potencia(b, n int) int {
+	if n == 1 {
+		return b
+	}
+	return b * potencia(b, n-1)
 }
