228ebeeec70c3a2a0c86d9299ac3545da71e2963
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-15 20:38:55 -0300

    Resolução Lista 04

--- lista04/questao02.go
+++ lista04/questao02.go
@@ -1,1 +1,30 @@
+package main
 
+import f "fmt"
+
+func main() {
+
+	f.Print("\nSoma de 10 números digitados pelo usuário.")
+	
+	var Array [10]float64
+	for i := 0; i < len(Array); i++ {
+
+		termo := 0.0
+		f.Print("\nDigite o %d número a ser somado:", i+1)
+		f.Scan(&termo)
+
+		Array [i] = termo
+	}
+
+	f.Printf("Array: %v", Array)
+
+	resultado := soma(Array)
+	f.Printf("\nA soma da Array é: %.2f\n\n", resultado)
+}
+
+func soma (array[] float64) float64 {
+		if len(array) == 0 {
+			return 0
+		}
+		return array[0] + soma(array[1:])
+	}
