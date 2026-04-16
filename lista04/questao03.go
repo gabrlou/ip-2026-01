0bfc69a1b4b4b54630b98e72aa5d6ca7b99c499e
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-16 00:15:43 -0300

    Resolução Lista 04

--- lista04/questao03.go
+++ lista04/questao03.go
@@ -1,1 +1,35 @@
+package main
 
+import f "fmt"
+
+func main() {
+
+	var quantidade int
+	f.Print("\nQuantos números serão inseridos? ")
+	f.Scan(&quantidade)
+
+	numeros := make([]int, quantidade)
+
+	for i := 0; i < quantidade; i++ {
+		f.Printf("Digite o %dº número: ", i+1)
+		f.Scan(&numeros[i])
+	}
+
+	inverterArray(numeros)
+
+	f.Println("\nArray invertido:")
+
+	for i := 0; i < quantidade; i++ {
+		f.Print(numeros[i], " ")
+	}
+}
+
+func inverterArray(numeros []int) {
+	if len(numeros) <= 1 {
+		return
+	}
+	numeros[0] = numeros[len(numeros)-1]
+	numeros[len(numeros)-1] = numeros[0]
+
+	inverterArray(numeros[1 : len(numeros)-1])
+}
