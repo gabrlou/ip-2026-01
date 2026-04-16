bb99b6405bf7874f39cefdcb30380a2a8cc05f1e
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-16 00:29:32 -0300

    Resolução Lista 04

--- lista04/questao04.go
+++ lista04/questao04.go
@@ -1,1 +1,32 @@
+package main
 
+import (
+	"fmt"
+	f "fmt"
+)
+
+func main() {
+
+	var numero int
+	fmt.Print("\nDigite um número: ")
+	fmt.Scan(&numero)
+
+	if numero == 0 {
+		fmt.Println("Número em binário: 0")
+		return
+	}
+
+	fmt.Print("Número em binário: ")
+	converter_binario(numero)
+	f.Print("\n\n")
+}
+
+func converter_binario(numero int) {
+	if numero == 0 {
+		return
+	}
+
+	converter_binario(numero / 2)
+
+	fmt.Print(numero % 2)
+}
