a324938903406b9fc28a5406d4fa1a9a81556e24
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-17 16:54:46 -0300

    Resolução Lista 03

--- Lista_03/questao33.go
+++ Lista_03/questao33.go
@@ -1,6 +1,23 @@
 package main
+
 import f "fmt"
 
 func main() {
-	var teste
+	var n1, n2 int
+
+	f.Print("\nDigite o valor de N1: ")
+	f.Scan(&n1)
+
+	f.Print("Digite o valor de N2: ")
+	f.Scan(&n2)
+
+	resto := n1
+	quociente := 0
+
+	for resto >= n2 {
+		resto -= n2
+		quociente++
+	}
+
+	f.Printf("\nResto (%d,%d) = %d\nQuociente (%d,%d) = %d\n\n", n1, n2, resto, n1, n2, quociente)
 }
