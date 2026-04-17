1102e7e8c379f25a9bf05212d1a241e043a0962b
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-17 13:36:37 -0300

    Resolução Lista 03

--- Lista_03/questao30.go
+++ Lista_03/questao30.go
@@ -1,6 +1,17 @@
 package main
-import f "fmt"
+
+import (
+	f "fmt"
+	m "math"
+)
 
 func main() {
-	var teste
+	var volume float64
+
+	for r := 0.0; r <= 20.0; r += 0.5 {
+
+		volume = (4.0 / 3.0) * m.Pi * m.Pow(r, 3)
+
+		f.Printf("Raio: %.1f cm | Volume: %.2f cm³\n", r, volume)
+	}
 }
