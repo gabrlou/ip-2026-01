bb76b4ef57eceba41f6f982bd5941fb9d5d104ee
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-17 16:16:08 -0300

    Resolução Lista 03

--- Lista_03/questao31.go
+++ Lista_03/questao31.go
@@ -1,6 +1,17 @@
 package main
-import f "fmt"
+
+import "fmt"
 
 func main() {
-	var teste
+	var graos, total uint64
+
+	graos = 1
+	total = 0
+
+	for i := 1; i <= 64; i++ {
+		total += graos
+		graos *= 2
+	}
+
+	fmt.Printf("\nTotal de grãos no tabuleiro: %d\n\n", total)
 }
