9de6ee1da74c2ce3b6a22b1860c16e5c1edde72c
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-19 17:34:52 -0300

    Resolução Lista 03

--- Lista_03/questao40.go
+++ Lista_03/questao40.go
@@ -1,6 +1,45 @@
 package main
-import f "fmt"
+
+import (
+	f "fmt"
+)
 
 func main() {
-	var teste
+	var (
+		preco        float64
+		ingressos    int
+		lucro        float64
+		lucro_max    float64
+		melhor_preco float64
+		melhor_quant int
+	)
+
+	f.Printf("\nTabela de lucro esperado:\n\n")
+	f.Printf("Preço\t\tIngressos\tLucro\n")
+
+	preco = 6.0
+	ingressos = 130
+
+	for preco >= 1.0 {
+
+		lucro = preco*float64(ingressos) - 300
+
+		f.Printf("R$ %.2f\t\t%d\t\tR$ %.2f\n", preco, ingressos, lucro)
+
+		// Verifica lucro máximo
+		if lucro > lucro_max || preco == 6.0 {
+			lucro_max = lucro
+			melhor_preco = preco
+			melhor_quant = ingressos
+		}
+
+		// Atualiza valores
+		preco -= 0.60
+		ingressos += 30
+	}
+
+	f.Printf("\nLucro máximo esperado\n\n")
+	f.Printf("	Lucro: R$ %.2f\n", lucro_max)
+	f.Printf("	Preço do ingresso: R$ %.2f\n", melhor_preco)
+	f.Printf("	Quantidade de ingressos: %d\n\n", melhor_quant)
 }
