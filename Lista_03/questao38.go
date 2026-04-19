daf16e699d2138e629a36a044d7ec5f9cb4f2a7d
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-19 17:19:13 -0300

    Resolução Lista 03

--- Lista_03/questao38.go
+++ Lista_03/questao38.go
@@ -1,6 +1,67 @@
 package main
+
 import f "fmt"
 
+func calculaDigito1(cpf []int) int {
+	soma := 0
+	peso := 2
+
+	for i := 8; i >= 0; i-- {
+		soma += cpf[i] * peso
+		peso++
+	}
+
+	resto := soma % 11
+	if resto < 2 {
+		return 0
+	}
+	return 11 - resto
+}
+
+func calculaDigito2(cpf []int, d1 int) int {
+	soma := 0
+	peso := 3
+
+	for i := 8; i >= 0; i-- {
+		soma += cpf[i] * peso
+		peso++
+	}
+
+	soma += d1 * 2
+
+	resto := soma % 11
+	if resto < 2 {
+		return 0
+	}
+	return 11 - resto
+}
+
 func main() {
-	var teste
+	var entrada string
+	cpf := make([]int, 11)
+
+	for {
+		f.Print("\nDigite o CPF (apenas números): ")
+		f.Scan(&entrada)
+
+		if len(entrada) != 11 {
+			f.Print("CPF inválido (digite um CPF com 11 dígitos).\n")
+			continue
+		} else {
+			break
+		}
+	}
+
+	for i := 0; i < 11; i++ {
+		cpf[i] = int(entrada[i] - '0')
+	}
+
+	d1 := calculaDigito1(cpf)
+	d2 := calculaDigito2(cpf, d1)
+
+	if d1 == cpf[9] && d2 == cpf[10] {
+		f.Print("\n	CPF válido!\n\n")
+	} else {
+		f.Print("\n	CPF inválido!\n\n")
+	}
 }
--- Lista_03/questao39.go
+++ Lista_03/questao39.go
@@ -1,6 +1,53 @@
 package main
+
 import f "fmt"
 
 func main() {
-	var teste
+
+	var (
+		id, peso                     int
+		maior_peso, menor_peso       int
+		id_maior_peso, id_menor_peso int
+	)
+
+	for i := 0; i < 90; i++ {
+
+		f.Printf("Boi nº %d - Digite o ID: ", i+1)
+		f.Scan(&id)
+
+		for {
+			f.Printf("Boi nº %d - Digite o peso: ", i+1)
+			f.Scan(&peso)
+
+			if peso <= 0 {
+				f.Println("Peso inválido. Digite um valor maior que zero.\n")
+				continue
+			} else {
+				break
+			}
+		}
+
+		if i == 0 {
+			maior_peso = peso
+			menor_peso = peso
+			id_maior_peso = id
+			id_menor_peso = id
+		} else {
+			// maior peso
+			if peso > maior_peso {
+				maior_peso = peso
+				id_maior_peso = id
+			}
+
+			// menor peso
+			if peso < menor_peso {
+				menor_peso = peso
+				id_menor_peso = id
+			}
+		}
+	}
+
+	f.Println("\n	RESULTADO FINAL:")
+	f.Printf("	Boi mais gordo: ID %d com %d kg\n", id_maior_peso, maior_peso)
+	f.Printf("	Boi mais magro: ID %d com %d kg\n", id_menor_peso, menor_peso)
 }
