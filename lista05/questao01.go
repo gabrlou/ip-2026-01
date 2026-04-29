bea1abf972eb9d3c9ed60e58a3505b0813ac7b49
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-04-29 15:45:55 -0300

    Solution 01

--- lista05/questao01.go
+++ lista05/questao01.go
@@ -3,6 +3,33 @@ package main
 import f "fmt"
 
 func main() {
-	var teste
-	f.Print("teste")
+	vetor := make([]int, 10)
+	maiorQue50 := make([]int, 10)
+	posicao := make([]int, 10)
+	existe := false
+	contador := 0
+
+	f.Print("\n")
+
+	for i := 0; i < 10; i++ {
+		f.Printf("Digite o %dº número para ser adicionado ao vetor: ", i+1)
+		f.Scan(&vetor[i])
+
+		if vetor[i] > 50 {
+			maiorQue50[contador] = vetor[i]
+			posicao[contador] = i + 1
+			existe = true
+			contador++
+		}
+	}
+
+	if existe {
+		f.Print("\nNúmero(s) > 50\t\tPosição no Vetor")
+		for i := 0; i < contador; i++ {
+			f.Printf("\n%d\t\t\t%d", maiorQue50[i], posicao[i])
+		}
+		f.Print("\n")
+	} else {
+		f.Print("\nNão existem números maiores que 50 no vetor preenchido.\n\n")
+	}
 }
