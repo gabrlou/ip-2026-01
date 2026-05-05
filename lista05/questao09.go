b2f3bf0739f9ddec662690744ba7a8b1f2e3e6e8
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-05-05 15:00:18 -0300

    Solutions

--- lista05/questao09.go
+++ lista05/questao09.go
@@ -3,8 +3,27 @@ package main
 import f "fmt"
 
 func main() {
-	var teste
-	f.Print("teste")
+    
+	alturas := make([]float64, 10)
+	var soma float64
+
+	f.Print("\n")
+	for i := 0; i < 10; i++ {
+		f.Printf("Digite a altura do %dº atleta: ", i+1)
+		f.Scan(&alturas[i])
+		soma += alturas[i]
+	}
+	
+	media := soma / 10
+
+	f.Printf("\nMédia das alturas: %.2f\n", media)
+	f.Print("Alturas acima da média:\n")
+
+	for i := 0; i < 10; i++ {
+		if alturas[i] > media {
+			f.Printf("%.2f ", alturas[i])
+		}
+	}
+
+	f.Print("\n\n")
 }
-
-
