8f787e648a50aecd9294ca24b054db5a42b30845
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-05-05 11:38:46 -0300

    Solutions

--- lista05/questao06.go
+++ lista05/questao06.go
@@ -3,8 +3,19 @@ package main
 import f "fmt"
 
 func main() {
-	var teste
-	f.Print("teste")
+
+	vetor := make([]int, 100)
+	j := 0
+
+	for i := 100; i >= 1; i-- {
+		vetor[j] = i
+		j++
+	}
+
+	f.Print("Vetor:\n")
+	for i := 0; i < 100; i++ {
+		f.Printf("%d ", vetor[i])
+	}
+
+	f.Print("\n\n")
 }
-
-
