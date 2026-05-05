14e60025f1f22642d9536fc56254927c7dccc11f
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-05-05 14:57:00 -0300

    Solutions

--- lista05/questao08.go
+++ lista05/questao08.go
@@ -1,10 +1,29 @@
 package main
 
-import f "fmt"
+import (
+	f "fmt"
+	m "math"
+)
 
 func main() {
-	var teste
-	f.Print("teste")
+    
+	vetor := make([]float64, 15)
+	var numero int
+
+	f.Print("\n")
+	for i := 0; i < 15; i++ {
+		f.Printf("Digite o %dº número: ", i+1)
+		f.Scan(&numero)
+
+		if numero < 0 {
+			vetor[i] = -1
+		} else {
+			vetor[i] = m.Sqrt(float64(numero))
+		}
+	}
+
+	
+	f.Print("\nValores armazenados:\n")
+	f.Printf("%.2f", vetor)
+	f.Print("\n\n")
 }
-
-
