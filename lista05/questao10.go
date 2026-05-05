0cf3663c4e63a21e8c6574ce0685f6c812b2457b
Gabriel Lourenço <gabriellourenco@discente.ufg.br>
2026-05-05 15:05:03 -0300

    Solutions
    Implement Fibonacci series calculation and printing.

--- lista05/questao10.go
+++ lista05/questao10.go
@@ -3,8 +3,24 @@ package main
 import f "fmt"
 
 func main() {
-	var teste
-	f.Print("teste")
+	const N = 50
+	fib := make([]int, N)
+
+	fib[0] = 1
+	fib[1] = 1
+
+	for i := 2; i < N; i++ {
+		fib[i] = fib[i-1] + fib[i-2]
+	}
+
+	f.Print("Série de Fibonacci:\n")
+	for i := 0; i < N; i++ {
+		if i == 0 {
+			f.Printf("%d", fib[i])
+		} else {
+			f.Printf(" - %d", fib[i])
+		}
+	}
+
+	f.Print("\n\n")
 }
-
-
